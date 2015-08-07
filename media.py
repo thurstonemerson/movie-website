bb1da44e5d8ca26db8c55391640cfb5af633392e
thurstonemerson <jmt.emerson@gmail.com>
2015-08-07 14:04:43 +0000

    Print out the lastest movie results from the TMDb API

--- media.py
+++ media.py
@@ -5,9 +5,17 @@ Created on 31/07/2015
 '''
 
 def get_latest_releases():
+
+    '''Retrieve movies from TMDb API that are now playing in theatres'''
+    
+    TMDb_connect = urllib.urlopen('https://api.themoviedb.org/3/movie/now_playing?api_key=23d81fdf84638b37ab9f8d898c12243c')
+    movie_json = json.loads(TMDb_connect.read())
+    TMDb_connect.close()
     
     movies = []
 
+    for result in movie_json['results']:           
+        print "{0}, {1}, {2}, {3}, {4}, {5}".format(result['id'], result['title'], result['overview'], result['poster_path'], result['release_date'], result['vote_average']))
         
     return movies
 
