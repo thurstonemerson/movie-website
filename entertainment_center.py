10c5d1f4f2e1aff56a40f5a1261952dd1f5e318c
thurstonemerson <jmt.emerson@gmail.com>
2015-08-18 14:55:13 +0000

    Call the function to open the html template

--- entertainment_center.py
+++ entertainment_center.py
@@ -13,6 +13,10 @@ def main():
 
     movies = media.get_latest_releases()
     
+    #open a webpage and display the list of movies
+    #trailers can be viewed via the poster image or view trailer button
+    media.open_movies_page(movies)
+    
     pass
 
 if __name__ == '__main__':
