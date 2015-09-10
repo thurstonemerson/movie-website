44d4efbcd8cf947793761858cf06796ec4718e41
thurstonemerson <jmt.emerson@gmail.com>
2015-09-10 20:34:34 +0000

    Added details regarding the module to media.py

--- media.py
+++ media.py
@@ -1,9 +1,21 @@
 '''
 Created on 31/07/2015
 
+This module can be used to retrieve/display in a webpage the latest movie releases
+now playing in theatres. At the moment only the first 20 results are displayed.
+Each movie has associated details such as overview, average
+popularity vote and release date. The movie trailer can be viewed by clicking on the
+poster or 'view trailer' button.
+
+This module  utilises the TMDb API, this requires attribution in the about section 
+(This product uses the TMDb API but is not endorsed or certified by TMDb)
+API key is 23d81fdf84638b37ab9f8d898c12243c
+
+At the moment this program is tightly bound to the TMDb API so some more steps are necessary
+to remove this dependence (in case we should require using a different API in future)
+
 @author: thurstonemerson
 '''
-
 import json
 import urllib
 import webbrowser
@@ -33,14 +45,30 @@ movie_tile_row_content = u'''
                      <button type="button" class="btn btn-default view-trailer" data-movie-id="{movie_id}" data-toggle="modal" data-target="#trailer">View trailer &raquo;</button>
                 </div><!-- /.div media-body-->
              </div><!-- /.div col-md-6-->
+'''
 
 
 def create_movie_tile_row(movie1, movie2):
     '''Creates a row in a bootstrap grid with two columns containing a movie tile each'''
 
-    content = ""
+    content = movie_tile_row_header
+    content += movie_tile_row_content.format(movie_title=movie1.title,
+                                         poster_image_url=movie1.poster_image_url,
+                                         movie_id=movie1.movie_id,
+                                         overview=movie1.overview,
+                                         vote_average=movie1.vote_average,
+                                         release_date=movie1.release_date)
+    content += movie_tile_row_content.format(movie_title=movie2.title,
+                                         poster_image_url=movie2.poster_image_url,
+                                         movie_id=movie2.movie_id,
+                                         overview=movie2.overview,
+                                         vote_average=movie2.vote_average,
+                                         release_date=movie2.release_date)
+    content += movie_tile_row_end
     
     return content
+
+
 
 
 def create_movie_tiles_content(movies):
@@ -55,6 +83,7 @@ def create_movie_tiles_content(movies):
         
     return content
 
+
 def open_movies_page(movies):
     '''This function opens main.html and appends the movie content'''
     
@@ -63,7 +92,7 @@ def open_movies_page(movies):
     #read the index page context and convert to unicode object
     main_page_content = unicode(main_file.read(), 'utf8')
     main_file.close()
-
+    
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
@@ -77,10 +106,9 @@ def open_movies_page(movies):
     # open the output file in the browser
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
-    
+
 
 def get_latest_releases():
-
     '''Retrieve movies from TMDb API that are now playing in theatres'''
     
     TMDb_connect = urllib.urlopen('https://api.themoviedb.org/3/movie/now_playing?api_key=23d81fdf84638b37ab9f8d898c12243c')
@@ -88,7 +116,7 @@ def get_latest_releases():
     TMDb_connect.close()
     
     movies = []
-
+    
     for result in movie_json['results']:           
         movie = Movie(result['id'], result['title'], result['overview'], result['poster_path'], result['release_date'], result['vote_average'])
         print movie.movie_id, movie.title.encode('utf8'), movie.poster_image_url.encode('utf8'), movie.release_date, movie.vote_average
@@ -96,6 +124,7 @@ def get_latest_releases():
         movies.append(movie)
         
     return movies
+
 
 class Movie():
     '''
@@ -124,4 +153,5 @@ class Movie():
         self.release_date = release_date
         self.vote_average = vote_average
         self.trailer_youtube_url = "http://www.youtube.com"
-
+        
+        
